2e094e82b9d93b2913481829e3509a402af5bcec
xsmartbartx <bw141105@gmail.com>
2025-03-31 17:31:01 +0200

    Create index.tsx

---
+++ src/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import './styles/modern.css';
+import './styles/theme.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { themeToggle } from './utils/themeToggle';
+
+// Initialize theme as early as possible to prevent flash of wrong theme
+themeToggle.initTheme();
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
